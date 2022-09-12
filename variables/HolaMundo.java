1adb11774c62726acc3885235aecf0b014770cc9
Alejandro Tonatiuh <53270727+alexsarget@users.noreply.github.com>
2022-09-11 22:01:36 -0500

    Agregando detalles al codigo hola mundo

--- variables/HolaMundo.java
+++ variables/HolaMundo.java
@@ -2,9 +2,15 @@ public class HolaMundo {
     public static void main(String[] args) {
         var usuario = "Juan";
         var titulo = "Ingeniero";
-
         var union = titulo + " " + usuario;
-
         System.out.println("union= " + union);
+
+        var i = 3;
+        var j = 4;
+
+        System.out.println(i + j);// Aqui se realiza la suma de numeros
+        System.out.println(i + j + usuario);//Evaluacion de izq a derecha, realiza suma
+        System.out.println(usuario + i + j);// contexto cadena
+        System.out.println(usuario + (i + j))//Uso de parentesis modifica la prioridad;
     }
 }
