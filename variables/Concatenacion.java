d2b6701bed4513c7a43f029beb08853ab29bf73b
Alejandro Tonatiuh <53270727+alexsarget@users.noreply.github.com>
2022-09-11 21:46:05 -0500

    Agregando ejercicio en la carpeta de proyectos

---
+++ variables/Concatenacion.java
@@ -0,0 +1,9 @@
+public class Concatenacion {
+    public static void main(String[]args){
+        var usuario ="Juan";
+        var titulo = "Ingeniero";
+
+        var union= titulo + usuario;
+        System.out.println("union= "+ union); 
+    }
+}
--- variables/HolaMundo.java
+++ variables/HolaMundo.java
@@ -1,23 +1,10 @@
-public class HolaMundo{
-    public static void main(String[]args){
-        //Definimos la variable
-        int miVarEnt=10;
-        System.out.println(miVarEnt);
-        //Modificamos el valor de la variable
-        miVarEnt = 5;
-        System.out.println(miVarEnt);
+public class HolaMundo {
+    public static void main(String[] args) {
+        var usuario = "Juan";
+        var titulo = "Ingeniero";
 
-        String miVarCad= "Saludos";
-        System.out.println(miVarCad);
+        var union = titulo + " " + usuario;
 
-        miVarCad="Adios";
-        System.out.println(miVarCad);
-
-        //var -Inferencias de tipos en Java
-        var miVarEnt2= 15;
-        System.out.println(miVarEnt2);
-
-        var miVarCad2="Hola Java";
-        System.out.println(miVarCad2);
+        System.out.println("union= " + union);
     }
 }
